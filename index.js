23f887e72c609da41c8ebfa686993d76c1db856c
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-06 10:50:24 -0400

    Passed all tests

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()* 10) +1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1,4,6,20)
